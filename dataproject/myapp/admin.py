1bc8537722537cf2d4886c4d0aef7c123b780110
Lingeswaran04 <Lingeswaran5315@gmail.com>
2023-01-13 20:23:46 +0000

    Success

--- dataproject/myapp/admin.py
+++ dataproject/myapp/admin.py
@@ -1,3 +1,3 @@
-from django.contrib import admin
-from .models import Employee,EmployeeAdmin
+from django.contrib import admin
+from .models import Employee,EmployeeAdmin
 admin.site.register(Employee,EmployeeAdmin)
